6ee4944fa991c0f1603e60353017b5208c2d25ce
Amine EL AMERI <el.ameri.amine@gmail.com>
2020-11-27 16:38:48 +0100

    partie 1 - 2

--- partie1.m
+++ partie1.m
@@ -10,11 +10,28 @@ fprintf("L'image est une matrice de %s entre %d et %d\n", class(img(10,10,2)), m
 fprintf("Elle est de dimension: %d*%d*%d\n", h, w, c);
 figure, imshow(img);
 
-% [x, y] = ginput(2);
-% fprintf('les points choisies sont (%i, %i) et (%i, %i)\n', x(1), y(1), x(2), y(2));
-x = [885;531];
-y = [175;5050000000000001e+02];
-Ln = norm([x(1)-x(2), y(1)-y(2)], 2);
+%[x, y] = ginput(2);
+%fprintf('les points choisies sont (%i, %i) et (%i, %i)\n', x(1), y(1), x(2), y(2));
+x = [4.409999999999999e+02;79];
+y = [56.999999999999940;3.869999999999999e+02];
+Ln = sqrt( (x(1)-x(2))^2 + (y(1)-y(2))^2 ); % longueur du segment
 
-U = 2*ceil(Ln) % Nombre de point a prendre
+U = 2*ceil(Ln); % Nombre de point a prendre
+
+Mu = zeros(U, 2);
+Mu(1,:) = [x(1) y(1)];
+Mu(U-1,:) = [x(2) y(2)];
+for u=2:U-2
+    Mu(u,:) = Mu(1,:) + (u/(U-1))*(Mu(U-1,:) - Mu(1,:));
+end
+
+%hold on
+%plot(Mu(:,1), Mu(:,2), '*');
+
+for u=1:U-1
+    pt_img = sum(img( ceil(Mu(u,1)), ceil(Mu(u,2)), :));
+    vect_afficher(u) = pt_img;
+end
+figure,
+plot(vect_afficher(1,:))
 
