7bd403a916b69f71c6da6e685bcafe06de3613de
Amine EL AMERI <el.ameri.amine@gmail.com>
2020-11-27 15:46:00 +0100

    partie 1 - 1

--- partie1.m
+++ partie1.m
@@ -1,10 +1,20 @@
-clear all;
-close all;
+clear all ;
+close all ;
 clc;
 
-img = imread("barcode1D.jpg");
-img2 = imread("rotated1DBarcode.jpg");
+%% Lecture
 
+img = imread('code_barre.png');
+[h, w, c] = size(img);
+fprintf("L'image est une matrice de %s entre %d et %d\n", class(img(10,10,2)), min(img(:)), max(img(:)));
+fprintf("Elle est de dimension: %d*%d*%d\n", h, w, c);
+%figure, imshow(img);
 
-figure, imshow(img);
-figure, imshow(img2);
+% [x, y] = ginput(2);
+% fprintf('les points choisies sont (%i, %i) et (%i, %i)\n', x(1), y(1), x(2), y(2));
+x = [885;531];
+y = [175;5050000000000001e+02];
+Ln = norm([x(1)-x(2), y(1)-y(2)], 2);
+
+U = 2*ceil(Ln) % Nombre de point a prendre
+
