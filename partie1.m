160a236d2374f13de3a76ca4c1af98d324f3f291
Anas BAHARI <anasbahari5@gmail.com>
2020-12-01 15:54:31 +0100

    crit

--- partie1.m
+++ partie1.m
@@ -5,15 +5,15 @@ clc;
 %% Lecture
 
 img = imread('code_barre2.png');
-[h, w, c] = size(img);
+[h, width, c] = size(img);
 fprintf("L'image est une matrice de %s entre %d et %d\n", class(img(10,10,2)), min(img(:)), max(img(:)));
-fprintf("Elle est de dimension: %d*%d*%d\n", h, w, c);
-%figure, imshow(img);
+fprintf("Elle est de dimension: %d*%d*%d\n", h, width, c);
+figure, imshow(img);
 
-%[x, y] = ginput(2);
-%fprintf('les points choisies sont (%i, %i) et (%i, %i)\n', x(1), y(1), x(2), y(2));
-x = [4.409999999999999e+02;79];
-y = [56.999999999999940;3.869999999999999e+02];
+[x, y] = ginput(2);
+% fprintf('les points choisies sont (%i, %i) et (%i, %i)\n', x(1), y(1), x(2), y(2));
+% x = [4.409999999999999e+02;79];
+% y = [56.999999999999940;3.869999999999999e+02];
 
 %% Lancer Aléatoire d'un rayon
 Ln = sqrt( (x(1)-x(2))^2 + (y(1)-y(2))^2 ); % longueur du segment
@@ -32,8 +32,36 @@ end
 signature_img(U-1) = sum( img( ceil(Mu(U-1,1)), ceil(Mu(U-1,2)), :) )/3;
 signature_img = signature_img(:);
 
-figure, plot(signature_img);
+% figure, plot(signature_img);
 
 %% Extraction de la signature le long d’un rayon
 figure, hist(signature_img, 256);
+
+
+hist_img = hist(signature_img, 256);
+
+N = 256;
+w = zeros(1,N);
+mu = zeros(1,N);
+denum = sum(hist_img);
+
+for i=1:N
+    w(i) = (sum(hist_img(1:i))) / denum;
+    mu(i) = (sum([1:i] .* hist_img(1:i))) / denum;
+end
+
+crit = w.*((mu(1,N)-mu).^2) + (1 - w).*mu.^2;
+[crit_max, k_max] = max(crit);
+
+
+figure, plot(signature_img);
+hold on,
+seuil = k_max*ones(length(signature_img));
+plot(seuil, 'g');
+
+signature_binaire = (signature_img>k_max);
+figure, plot(signature_binaire, 'r-');
+
+
+
 
